fbc4d51a09e6776514ab15b98b52c208ea3adf86
NOK <92591333+potatochick2020@users.noreply.github.com>
2022-11-10 08:08:40 +0000

    Update App.js

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 import axios from "axios"
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function getUser() {
   let rval = {}
@@ -35,21 +35,61 @@ function getPost() {
 }
 
 function App() {
-  const[user, setUser] = useState(getUser())
-  const[post, setPost] = useState(getPost())
-
   
+  const [chartData, setChartData] = useState({});
+  const[user, setUser] = useState([])
+  const[post, setPost] = useState([])
+  
+  const chart = () => {
+    let users = [];
+    let posts = [];
+    let rval = [];
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => { 
+        response.data.forEach(element => {
+          rval[element.userId]=(rval[element.userId] || 0) + 1;
+        }); 
+        for (const [key, value] of Object.entries(rval)){
+          posts.push(value);
+        }
+        setChartData({
+          x: users,
+          y: posts,
+          type: 'bar'
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(function (response) {
+      response.data.forEach(element => {
+        rval[element.id] = (element.name)
+      }); 
+      for (const [key, value] of Object.entries(rval)){
+        users.push(value);
+      } 
+      setChartData({
+        x: users,
+        y: posts,
+        type: 'bar'
+      });
+    }) 
+       
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+
     return (
       <div className = "Container-fluid">
         <div className = "row"> 
           <div className = "col">
           <Plot
         data={[ 
-          {
-            x: getUser(),
-            y: getPost(),
-            type: 'bar'
-          },
+          {chartData},
         ]}
         // layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
       />
